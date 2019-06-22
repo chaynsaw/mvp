2a36c893cb305899849fcba1817d719683465e36
Chaynor Hsiao <chaynorhsiao@Chaynors-MacBook-Pro.local>
2019-06-22 15:02:32 -0700

    Implement Create Task conditional render onClick

--- react-client/src/CreateTask.jsx
+++ react-client/src/CreateTask.jsx
@@ -3,10 +3,11 @@ import $ from 'jquery';
 class CreateTask extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {clicked:false};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clicked = this.clicked.bind(this);
   }
 
   handleChange(event) {
@@ -24,40 +25,53 @@ class CreateTask extends React.Component {
     })
   }
 
-  render() {
-    return (
-      <div>
-        <h2>Create a Task</h2>
-        <form onSubmit={this.handleSubmit} action ="/" method="POST">
-          <label name="title" >
-            What do you need to work on? 
-            <br />
-            <input type="text" name="name" id="name" value={this.state.value} onChange={this.handleChange} placeholder="Enter task name" size="40" required/>
-          </label>
-            <br />
-          <label name="description" >
-            Description:
-            <br />
-            <textarea name="description" style={{'width':'250px','height':'150px',}} value={this.state.value} onChange={this.handleChange} placeholder="Enter what this task is for (optional)"></textarea>
-          </label>
-            <br />
-          <label for="importance">Rate the importance of this remediation:</label>
-            <br />
-            <select id="importance" name="importance" value={this.state.value} onChange={this.handleChange} required>
-                <option value="">--Please choose an option--</option>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <br />
+  clicked() {
+    this.setState({clicked:!this.state.clicked})
+  }
 
-            <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
+  render() {
+    if (this.state.clicked) {
+      return (
+        <div>
+          <h2 onClick={this.clicked}>Create a Task</h2>
+          <form onSubmit={this.handleSubmit} action ="/" method="POST">
+            <label name="title" >
+              What do you need to work on? 
+              <br />
+              <input type="text" name="name" id="name" value={this.state.value} onChange={this.handleChange} placeholder="Enter task name" size="40" required/>
+            </label>
+              <br />
+            <label name="description" >
+              Description:
+              <br />
+              <textarea name="description" style={{'width':'250px','height':'150px',}} value={this.state.value} onChange={this.handleChange} placeholder="Enter what this task is for (optional)"></textarea>
+            </label>
+              <br />
+            <label htmlFor="importance">Rate the importance of this remediation:</label>
+              <br />
+              <select id="importance" name="importance" value={this.state.value} onChange={this.handleChange} required>
+                  <option value="">--Please choose an option--</option>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+              </select>
+              <br />
+  
+              <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2 onClick={this.clicked}>Create a Task</h2>
+        </div>
+      )
+    }
+    
   }
 }
 
